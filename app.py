305bd8410701c7428d31a4d80274cf4fd802c589
oluwasube <enibeke20@gmail.com>
2023-06-12 12:23:14 +0100

    fixed

--- app.py
+++ app.py
@@ -1,52 +1,68 @@
-from flask import Flask, render_template
-import csv
+import os
+import datetime
+from flask import Flask
+from dash import Dash, dcc, html
 import plotly.graph_objects as go
+import pandas as pd
 
-app = Flask(__name__, template_folder='templates')
 
 
-@app.route("/")
-def display_data():
-    # Read candlestick data from CSV
-    filename = "BTCUSDT_1h.csv"
+# Create Flask app
+server = Flask(__name__)
+app = Dash(__name__, server=server)
 
-    with open(filename, "r") as file:
-        reader = csv.reader(file)
-        data = list(reader)
+directory = "data"
+files = os.listdir(directory)
+csv_files = [file for file in files if file.endswith('.csv')]
 
-    open_time = [row[0] for row in data[1:]]
-    close = [float(row[4]) for row in data[1:]]
-    high = [float(row[2]) for row in data[1:]]
-    low = [float(row[3]) for row in data[1:]]
-    Open = [float(row[1]) for row in data[1:]]
+if len(csv_files) > 0:
 
-    # Create candlestick chart
-    candlestick_chart = go.Candlestick(
-        x=open_time, open=Open, high=high, low=low, close=close
+    filename = csv_files[0]
+    data_path = f"{directory}/{filename}"
+    data = pd.read_csv(data_path)
+else:
+    data = pd.DataFrame()
+
+
+# Define layout
+app.layout = html.Div([
+    html.H1('Candlestick Chart'),
+    dcc.Graph(
+        id='candlestick-chart',
+        figure={
+            'data': [
+                go.Candlestick(
+                    x=pd.to_datetime(data['Open_Time'], unit='ms'),
+                    open=data['Open'],
+                    high=data['High'],
+                    low=data['Low'],
+                    close=data['Close']
+                )
+            ],
+            'layout': go.Layout(
+                title='BTCUSDT Candlestick Chart',
+                xaxis=dict(title='Date'),
+                yaxis=dict(title='Price')
+            )
+        }
+    ),
+    html.H1('Market Cap Pie Chart'),
+    dcc.Graph(
+        id='market-cap-pie-chart',
+        figure={
+            'data': [
+                go.Pie(
+                    labels=["BTC", "ETH", "XRP", "LTC", "BCH",
+                            "ADA", "DOT", "LINK", "XLM", "DOGE"],
+                    values=[1000, 800, 600, 500, 400, 300, 200, 150, 100, 50]
+                )
+            ],
+            'layout': go.Layout(
+                title='Market Cap',
+            )
+        }
     )
+])
 
-    # Create pie chart
-    symbols = ["BTC", "ETH", "XRP", "LTC", "BCH",
-               "ADA", "DOT", "LINK", "XLM", "DOGE"]
-    market_caps = [1000, 800, 600, 500, 400, 300, 200, 150, 100, 50]
-
-    pie_chart = go.Pie(labels=symbols, values=market_caps)
-
-    layout = go.Layout(
-        title='Candlestick Data and Market Caps',
-        xaxis=dict(title='Date'),
-        yaxis=dict(title='Price'),
-        showlegend=False
-    )
-
-    fig = go.Figure(data=(candlestick_chart, pie_chart), layout=layout)
-
-    # Convert the figure to JSON for rendering
-    plot_div = fig.to_json()
-
-    return render_template('index.html', plot_div=plot_div)
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ utils/get_market_cap.py
@@ -0,0 +1,37 @@
+import requests
+
+
+def get_market_cap():
+    url = 'https://api.binance.com/api/v3/ticker/price'
+    params = {
+        'symbol': 'BTCUSDT,ETHUSDT,XRPUSDT,LTCUSDT,BCHUSDT,ADAUSDT,DOTUSDT,LINKUSDT,XLMUSDT,DOGEUSDT'}
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        market_cap_data = response.json()
+        market_cap_values = []
+        for item in market_cap_data:
+            symbol = item['symbol']
+            price = float(item['price'])
+            circulating_supply = get_circulating_supply(symbol)
+            if circulating_supply:
+                market_cap = price * circulating_supply
+                market_cap_values.append(market_cap)
+        return market_cap_values
+    else:
+        return None
+
+
+def get_circulating_supply(symbol):
+    url = f'https://api.coingecko.com/api/v3/coins/{symbol.lower()}'
+    response = requests.get(url)
+    print(response)
+    if response.status_code == 200:
+        data = response.json()
+        circulating_supply = float(data['market_data']['circulating_supply'])
+        return circulating_supply
+    else:
+        return None
+
+
+market_cap = get_market_cap()
+print(market_cap)
